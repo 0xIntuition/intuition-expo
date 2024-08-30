e607f7b48b0244fc273252573d8c987f97bc85f7
Simonas Karuzas <simonas@not.cat>
2024-08-30 18:11:43 +0300

    feat: basic atom deposit redeem

--- app/a/[id].tsx
+++ app/a/[id].tsx
@@ -1,30 +1,81 @@
-import { Button, View } from 'react-native';
+import { Button, View, TouchableOpacity } from 'react-native';
 import { Image, StyleSheet } from 'react-native';
 import { Link, Stack, useLocalSearchParams } from 'expo-router';
 import { useQuery, gql } from '@apollo/client';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { shareAsync } from 'expo-sharing';
-import { useCallback, useMemo, useState } from 'react';
+import { useState } from 'react';
+import { formatEther, parseEther } from 'viem';
+import { useMultiVault } from '@/hooks/useMultiVault';
+import { Section } from '@/components/section';
+import { ListItem } from '@/components/list-item';
 import { useWalletConnectModal } from '@walletconnect/modal-react-native';
-import { Address, createPublicClient, createWalletClient, custom, http, parseEther } from 'viem';
-import { base } from 'viem/chains';
-import { Multivault } from '@/lib/protocol';
 
 const GET_ATOM = gql`
-query Atom ($id: BigInt!){
+query Atom($id: BigInt!, $address: String) {
   atom(id: $id) {
     id
     label
     image
     emoji
     type
+    vault {
+      totalShares
+      positionCount
+      currentSharePrice
+      positions(orderBy: "shares", orderDirection: "desc") {
+        items {
+          shares
+          account {
+            id
+            image
+            label
+          }
+        }
+      }
+    }
+    value {
+      thing {
+        description
+        image
+        name
+        url
+      }
+      person {
+        image
+        name
+        url
+        description
+      }
+      organization {
+        image
+        name
+        url
+        description
+      }
+    }
+  }
+  chainlinkPrices(limit: 1, orderBy: "id", orderDirection: "desc") {
+    items {
+      usd
+    }
+  }
+  positions(where: { accountId: $address, vaultId: $id }, limit: 1) {
+    items {
+      shares
+    }
   }
 }`;
 export default function Atom() {
   const { id } = useLocalSearchParams();
-  const { loading, error, data, refetch } = useQuery(GET_ATOM, { variables: { id } });
-  const { open, isConnected, address, provider } = useWalletConnectModal();
+  const { address } = useWalletConnectModal();
+  const { loading, error, data, refetch } = useQuery(GET_ATOM, {
+    fetchPolicy: 'network-only',
+    variables: { id, address: address }
+  });
+  const [signalInProgress, setSignalInProgress] = useState(false);
+  const multivault = useMultiVault();
   const [errorMesage, setErrorMesage] = useState<string | null>(null);
 
   if (loading) return <ThemedText>Loading...</ThemedText>;
@@ -33,50 +84,52 @@ export default function Atom() {
   if (!data.atom) {
     return <ThemedText>Atom not found</ThemedText>;
   }
-
   const { atom } = data;
 
   const handleDeposit = async () => {
 
-    if (!isConnected || !address || !provider) {
-      open();
+    if (!multivault) {
+      setErrorMesage('Connect wallet first');
       return;
     }
 
-    const publicClient = createPublicClient({
-      chain: base,
-      transport: http(),
-    })
-
-    const walletClient = createWalletClient({
-      chain: base,
-      account: address as Address,
-      transport: custom({
-        async request({ method, params }) {
-          return await provider?.request({ method, params });
-        },
-      }),
-    })
-
-    const multivault = new Multivault({
-      // @ts-ignore
-      publicClient,
-      // @ts-ignore
-      walletClient,
-    });
-
+    setSignalInProgress(true);
 
     multivault?.depositAtom(BigInt(atom.id), parseEther('0.00042'))
-      .then(({ shares }) => {
-        setErrorMesage(`Bought ${shares} shares`);
+      .then(() => {
+        refetch();
       })
       .catch((error) => {
         console.error(error);
         setErrorMesage(error.message);
+      }).finally(() => {
+        setSignalInProgress(false);
       });
-
   };
 
+  const handleWithdraw = async () => {
+
+    if (!multivault) {
+      setErrorMesage('Connect wallet first');
+      return;
+    }
+
+    multivault?.redeemAtom(BigInt(atom.id), parseEther('0.00042'))
+      .then(() => {
+        refetch();
+      })
+      .catch((error) => {
+        console.error(error);
+        setErrorMesage(error.message);
+      }).finally(() => {
+        setSignalInProgress(false);
+      });
+  };
+
+  const description = atom.value?.thing?.description || atom.value?.person?.description || atom.value?.organization?.description || '';
+  const totalStaked = parseFloat(formatEther(atom.vault.totalShares))
+    * parseFloat(formatEther(atom.vault.currentSharePrice));
+  const usd = data.chainlinkPrices.items[0].usd;
 
   return (
     <ThemedView style={styles.container}>
@@ -88,13 +141,54 @@ export default function Atom() {
           </View>,
         }}
       />
-      <ThemedText>{atom.emoji} {atom.type}</ThemedText>
-      <Button title="Share" onPress={async () => {
-        await shareAsync('https://i7n.app/a/' + id);
-      }} />
+      <Section >
+        <ListItem
+          label={description}
+          subLabel={`${atom.type} ∙ ID: ${atom.id}`}
+        />
+        {data.positions.items.length > 0 && (
+          <ListItem
+            label="My stake"
+            value={`${(
+              parseFloat(formatEther(atom.vault.currentSharePrice))
+              * parseFloat(formatEther(data.positions.items[0].shares))
+              * usd).toFixed(2)} USD`}
+          />
+        )}
+        <ListItem
+          label="Total staked"
+          subLabel={`Holders: ${atom.vault.positionCount}`}
+          value={`${(totalStaked * usd).toFixed(2)} USD`}
+          last
+        />
 
-      <Button title="Deposit" onPress={handleDeposit} />
+      </Section>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingRight: 8, paddingLeft: 8 }}>
+        {signalInProgress && <ThemedText>Signal in progress...</ThemedText>}
+        {!signalInProgress && <Button title="Deposit" onPress={handleDeposit} />}
+        {!signalInProgress && data.positions.items.length > 0 && <Button title="Withdraw" onPress={handleWithdraw} />}
+        <Button title="Share" onPress={async () => {
+          await shareAsync('https://i7n.app/a/' + id);
+        }} />
+      </View>
+
       <ThemedText>{errorMesage}</ThemedText>
+      {errorMesage && <Link href={{ pathname: '(tabs)/me' }}><ThemedText>Go to me</ThemedText></Link>}
+      <Section title="Top Holders">
+        {atom.vault.positions.items.map(({ shares, account }: any) => (
+          <ListItem
+            key={account.id}
+            id={account.id}
+            image={account.image}
+            label={account.label}
+            value={`${(
+              parseFloat(formatEther(atom.vault.currentSharePrice))
+              * parseFloat(formatEther(shares))
+              * usd).toFixed(2)} USD`}
+          />
+        ))}
+      </Section>
 
     </ThemedView>
   );
@@ -105,7 +199,6 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   image: {
     width: 32,
