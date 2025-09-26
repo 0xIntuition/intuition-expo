7127968687221ae78fe3092f50dca6bcdaafac66
Simonas Karuzas <simonas@not.cat>
2025-09-26 13:14:41 +0300

    feat: colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,17 +3,19 @@ const tintColorDark = '#fff';
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
-    secondaryBackground: '#f5f5f5',
+    text: 'rgb(0,0,0)',
+    background: 'rgb(242,242,247)',
+    secondaryBackground: 'rgb(255,255,255)',
+    border: 'rgb(232,232,232)',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
-    secondaryBackground: '#1a1a1a',
+    text: 'rgb(255,255,255)',
+    background: 'rgb(0,0,0)',
+    secondaryBackground: 'rgb(28,28,30)',
+    border: 'rgb(56,56,59)',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
